a6def7836848bde6ec44298532db64a278cab334
Avnish Kumar <avnein4988@gmail.com>
2024-07-22 12:07:22 +0530

    create the functionality to show all the previous mock onterview of the user

--- src/app/dashboard/_components/InterviewList.jsx
+++ src/app/dashboard/_components/InterviewList.jsx
@@ -1,19 +1,41 @@
-'use client'
-import React from 'react'
-import { getInterviewDetails, getInterviewList } from '../../../../utils/db'
-import { useUser } from '@clerk/nextjs';
+"use client";
+import React, { useState, useEffect } from "react";
+import { getInterviewDetails } from "../../../../utils/db";
+import { useUser } from "@clerk/nextjs";
+import InterviewItemCard from "./InterviewItemCard";
+import { db } from "../../../../utils/db";
+import { eq, desc } from "drizzle-orm";
+import { MockInterview } from "../../../../utils/schema";
 function InterviewList() {
+  const user = useUser();
+  const userEmail = user?.user?.primaryEmailAddress?.emailAddress;
+  const [interviewList, setInterviewList] = useState();
+  console.log(userEmail);
 
-    const user=useUser();
-    const userEmail=user?.user?.primaryEmailAddress?.emailAddress;
-    console.log(userEmail)
-    const interViewList= getInterviewList(userEmail);
-    console.log(interViewList);
+  useEffect(() => {
+    user && getInterviewList();
+  }, [userEmail]);
+
+  const getInterviewList = async () => {
+    const result = await db
+      .select()
+      .from(MockInterview)
+      .where(eq(MockInterview.createdBy, userEmail))
+      .orderBy(desc(MockInterview.id));
+
+    setInterviewList(result);
+
+    console.log(result);
+  };
   return (
     <div>
-        <h2 className='font-bold text-lg'>Previous Mock Interview</h2>
+      <h2 className="font-bold text-lg">Previous Mock Interview</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-3">
+      {interviewList &&
+        interviewList.map((interview, index) => <InterviewItemCard interview={interview} key={index}/>)}
+      </div>
     </div>
-  )
+  );
 }
 
-export default InterviewList
+export default InterviewList;
--- src/app/dashboard/interview/[id]/feedback/page.jsx
+++ src/app/dashboard/interview/[id]/feedback/page.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../../../../../utils/db'
+import { UserAnswer } from '../../../../../../utils/schema'
+import { eq } from 'drizzle-orm'
+import Link from 'next/link'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import { ChevronsUpDown } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
-function Feedback() {
+
+function Feedback({params}) {
+  
+  const[feedbackList,setfeedbackList]=useState(null);
+  useEffect(()=>{
+    getFeedback();
+  },[])
+
+  const getFeedback=async()=>{
+    const result=await db.select()
+                         .from(UserAnswer)
+                         .where(eq(UserAnswer.mockIdRef,params.id))
+                         .orderBy(UserAnswer.id)
+
+    console.log(result)
+    setfeedbackList(result)
+  }
+
+  
   return (
-    <div>Feedback</div>
+    <div className='p-10'>
+      <h2 className='text-3xl font-bold text-green-500'>Congratulations!</h2>
+      <h2 className='font-bold text-2xl'>Here is your interview feedback</h2>
+      <h2 className='text-lg my-3 text-purple-800'>Your overall rating:<strong>7/10</strong></h2>
+
+      <h2 className='text-sm text-gray-500'>Find below interview question with correct answer, Your answer and feedback for improvement</h2>
+
+      {
+        feedbackList&&feedbackList.map((item,index)=>(
+          <Collapsible  className="mt-7" key={index}>
+          <CollapsibleTrigger className='p-2 bg-secondary rounded-lg my-2 text-left flex justify-between gap-10 w-full'>{item.question} <ChevronsUpDown className='h-5 w-5'/></CollapsibleTrigger>
+          <CollapsibleContent>
+            <div className='flex flex-col gap-2'>
+              <h2 className='text-red-500 p-2 border rounded-lg'><strong>Rating:</strong>{item.rating}</h2>
+              <h2 className='bg-red-50 p-2 border rounded-lg text-sm text-red-900'><strong>User Answer:</strong>{item.userAns}</h2>
+              <h2 className='bg-green-50 p-2 border rounded-lg text-sm text-green-900'><strong>Correct Answer:</strong>{item.correctAns}</h2>
+              <h2 className='bg-blue-50 p-2 border rounded-lg text-sm text-purple-800'><strong>Feedback:</strong>{item.feedback}</h2>
+            </div>
+          </CollapsibleContent>
+        </Collapsible>
+        ))
+      }
+      <Link href={'/dashboard'}>
+      <Button>Go Home</Button>
+      </Link>
+    </div>
   )
 }
 
