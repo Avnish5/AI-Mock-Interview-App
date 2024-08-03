fad6b456d8f12748a707719ee1793e128e328f2f
Avnish Kumar <avnein4988@gmail.com>
2024-08-03 13:13:52 +0530

    edit the nextconfig file

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,7 @@ const nextConfig = {
     compiler:{
         removeConsole: process.env.NODE_ENV === "production"
     },
-    generateBuildId: async () => {
-        // This could be anything, using the latest git hash
-        return process.env.GIT_HASH
-      },
+    
 };
 
 export default nextConfig;
